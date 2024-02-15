61dca7550781c2e08a0dbfb35ea3602016cd235b
Aastha Saxena <aasthasaxena100@gmail.com>
2024-02-15 00:42:38 -0500

    newItem form

--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 const NewItem = () => {
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(e.target)
+
+  }
   return (
-    <div className='newItem m-5 '>
-        <form className="flex gap-2 justify-center" id="newItemForm" >
-          <input className="p-1 w-60" type="text" name="item" id="" placeholder="Bread...." />
+    <div className='newItem m-10 '>
+      <legend>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 justify-center" id="newItemForm" >
+          
+          <label for="itemName">Item:</label>
+          <input id="itemName" className="p-1 w-full" type="text" name="item" placeholder="Peas" required/>
+          <label for="itemDetails">Details:</label>
+          <textarea id="itemDetails" className="p-1 w-full" type="text" name="item" placeholder="2 lbs" ></textarea>
+        
             
-            <button className="bg-black text-white px-4 py-2 rounded-2xl"type="submit">Add an item!</button>
+            <button  className="bg-black text-white px-4 py-2 rounded-2xl"type="submit">Add an item!</button>
+            
         </form>
+        </legend>
       
     </div>
   )
